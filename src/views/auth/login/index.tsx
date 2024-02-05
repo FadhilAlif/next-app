ac349b524ca6d378b786eb20fb3ea50fde858267
FadhilAlif <92931140+FadhilAlif@users.noreply.github.com>
2024-02-05 14:20:45 +0700

    [FIX] : "callbackUrl"

--- src/views/auth/login/index.tsx
+++ src/views/auth/login/index.tsx
@@ -4,7 +4,7 @@ import { redirect } from "next/dist/server/api-utils";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { query as queryType } from "next/router";
+// import { query as queryType } from "next/router";
 
 const LoginViews = () => {
   const [isLoading, setIsLoading] = useState(false);
