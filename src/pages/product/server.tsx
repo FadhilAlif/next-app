64c44766d5f02f92d0028c33a2040facd482b323
FadhilAlif <92931140+FadhilAlif@users.noreply.github.com>
2024-02-05 14:13:33 +0700

    [UPDATE] : "Change env API URL"

--- src/pages/product/server.tsx
+++ src/pages/product/server.tsx
@@ -14,7 +14,7 @@ export default ProductPage;
 // TODO : Server Side Request
 export async function getServerSideProps() {
   // Fetch Data
-  const res = await fetch("http://localhost:3000/api/product");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/product`);
   const response = await res.json();
 
   return {
